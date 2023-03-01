6773bed038147a6d61482d749c753db493a21c28
Anthony Rocha <Anthony.Rocha@amd.com>
2023-02-28 18:20:18 -0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+#![no_std]
+#![no_main]
+
+use core::arch::global_asm;
+use core::ptr;
+
+global_asm!(include_str!("start.S"));
+
+const OUT_STR: &'static [u8; 14] = b"Hello Caliptra";
+static mut COUNT: u8 = 0x41;
+
+#[no_mangle]
+pub extern "C" fn main() {
+    const UART0: *mut u8 = 0x2000_1041 as *mut u8;
+    unsafe {
+        for byte in OUT_STR {
+            ptr::write_volatile(UART0, COUNT);
+            ptr::write_volatile(UART0, *byte);
+            COUNT = COUNT + 1;
+        }
+        ptr::write_volatile(UART0, b'\n');
+    }
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
